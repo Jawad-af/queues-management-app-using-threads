192f719a85d8ab2c640b731e412acf4971ffa0b8
Jawad-af <aboufakherj@gmail.com>
2024-04-05 23:04:46 +0300

    preImplementation commit

--- src-code-java-maven/queues-management-application/src/main/java/org/queuesmgmtapp/Main.java
+++ src-code-java-maven/queues-management-application/src/main/java/org/queuesmgmtapp/Main.java
@@ -1,4 +1,8 @@
 package org.queuesmgmtapp;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Main {
     public static void main(String[] args) {
 
